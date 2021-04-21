3beb5f34cfef5bd4ce03d35c195eb699c57df2fd
Daniel Muldoon <36717797+danielmuldoon@users.noreply.github.com>
2021-04-21 18:31:03 -0400

    Update rna-star-groups-dge.sh

--- routes/rna-star-groups-dge.sh
+++ routes/rna-star-groups-dge.sh
@@ -150,7 +150,7 @@ sleep 3
 echo -e "\n ========== test R environment ========== \n"
 
 # load relevant modules
-module add r/4.0.0
+module add r/4.0.3
 
 echo
 echo " * R: $(readlink -f $(which R)) "
